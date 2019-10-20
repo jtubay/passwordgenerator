cc3eb67a90e94fbf0f8caf2d3a89bc282c7bbd45
Jean Tubay <jean.tubay.92@gmail.com>
2019-10-20 12:29:44 -0400

    created the generate pw function

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
+//function to generate password
 
-function generatePassword() {
-     var length = 8,
-            charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-            retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-            retVal += charset.charAt(Math.floor(Math.random() * n));
-        }
-    return retVal;
-}
-console.log(generatePassword())
-alert(generatePassword())
+function generate () {
+
+    //alert to choose a length
+    var passwordLength = prompt('Please pick a length between 8 to 128').value;
+
+    // final password
+    var finalPassword = '';
+
+    
+    //prompt asking for paramiters 
+    var choiceOne = prompt('Do you want capital letters?')
+    var choiceTwo = prompt('Do you want Lower case letters?')
+    var choiceThree = prompt('Do you want special characters?')
+    var choiceFour = prompt('Do you want capital symbols?')
+    
+    //arrays 
+    var outcomeOne = 'ABCDEFGHIJKLMNOPQRSTUVWYXZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+'
+
+    for (var i = 8; i <= passwordLength;  i++) {
+        finalPassword = finalPassword + outcomeOne.charAt(Math.floor(Math.random() * outcomeOne.length))
+    }
+    
+    document.getElementById('display').value = finalPassword;
+}    
+
+
+
+
+
+
+
+// function generatePassword() {
+//      var length = 8,
+//             charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+//             retVal = "";
+//     for (var i = 0, n = charset.length; i < length; ++i) {
+//             retVal += charset.charAt(Math.floor(Math.random() * n));
+//         }
+//     return retVal;
+// }
+// console.log(generatePassword())
+// alert(generatePassword())
