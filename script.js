a0690406af2f2fac8261878f2a6a5ddbb4b6bf4b
Jean Tubay <jean.tubay.92@gmail.com>
2019-10-20 13:21:13 -0400

    added slider to allow user to determine lenght. used an onclick to generate pw

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-//function to generate password
+// //function to generate password
 
-function generate () {
 
-    //alert to choose a length
-    var passwordLength = prompt('Please pick a length between 8 to 128').value;
 
-    // final password
-    var finalPassword = '';
+// function generate () {
+
+//     //alert to choose a length
+    
+//     let comlexity = document.getElementById('slider').value;
+//     // final password
+//     let password = '';
 
     
-    //prompt asking for paramiters 
-    var choiceOne = prompt('Do you want capital letters?')
-    var choiceTwo = prompt('Do you want Lower case letters?')
-    var choiceThree = prompt('Do you want special characters?')
-    var choiceFour = prompt('Do you want capital symbols?')
+//     //prompt asking for paramiters 
+//     // var choiceOne = prompt('Do you want capital letters?')
+//     // var choiceTwo = prompt('Do you want Lower case letters?')
+//     // var choiceThree = prompt('Do you want special characters?')
+//     // var choiceFour = prompt('Do you want capital symbols?')
     
-    //arrays 
-    var outcomeOne = 'ABCDEFGHIJKLMNOPQRSTUVWYXZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+'
+//     //arrays 
+//     const values = 'ABCDEFGHIJKLMNOPQRSTUVWYXZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+'
 
-    for (var i = 8; i <= passwordLength;  i++) {
-        finalPassword = finalPassword + outcomeOne.charAt(Math.floor(Math.random() * outcomeOne.length))
-    }
+//     for (var i = 0; i <= complexity; i++) {
+//         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+//     }
     
-    document.getElementById('display').value = finalPassword;
-}    
+//     document.getElementById('display').value = password;
+// }    
 
+// //set the default length 
+// document.getElementById('pwLength').innerHTML = "Length: 8";
+
+// document.getElementById('slider').oninput = function () {
+//     if(document.getElementById('slider').value >= 8) {
+//         document.getElementById('pwLength').innerHTML = 'Length: ' + document.getElementById('slider').value;
+//     } else {
+//         document.getElementById('pwLength').innerHTML= 'length: 8'
+//     }
+// }
 
 
 
@@ -42,3 +54,42 @@ function generate () {
 // }
 // console.log(generatePassword())
 // alert(generatePassword())
+
+
+
+/// generato des
+function generate() {
+
+    //set pw length/complexity
+    let complexity =document.getElementById('slider').value;
+    //possible values
+    const values = "ABCDEFGHIJKLMNOPQRSTUVWYXZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+"
+
+    let password = '';
+
+    //creat for loop to chose pw cha
+    for (var i = 0; i < complexity; i++) {
+        password = password + values[Math.floor(Math.random() * values.length)];
+    }
+
+    // add password to textbox/display are
+    document.getElementById('display').value = password;
+
+//     // add pw to prev generated section
+//     document.getElementById('lastNums').innerHTML += password + '<br />';
+ }
+
+
+//set default length display 
+// document.getElementById('length').innerHTML = "Length: 8";
+
+// function to set length based on slider position
+document.getElementById('slider').oninput = function() {
+
+    if(document.getElementById('slider').value >= 8) {
+        document.getElementById('length').innerHTML = 'Length: ' + document.getElementById('slider').value;
+    } else {
+        document.getElementById('length').innerHTML = "length = 8"
+    }
+
+}
